71bec7dbf9423adc4bdcf810c7552ad4a2642dee
Shafayat Bin Mamun <mamunshafayatbin@yahoo.com>
2026-01-05 18:02:17 +0600

    7a. Added sword slash animation

--- enemy/enemy.gd
+++ enemy/enemy.gd
@@ -2,6 +2,7 @@ extends CharacterBody2D
 
 @onready var player_node: CharacterBody2D = get_parent().get_node("Player")
 @onready var hit_area: Area2D = $HitArea
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
 
 @export var health: float = 3.0
 
@@ -24,3 +25,9 @@ func _hit(body: Node2D)-> void:
 		var knockback_direction = (body.global_position - global_position).normalized()
 		body.knockback_state.apply_knockback(knockback_direction, 500.0, 0.55)
 		body.state_machine.change_state(StateTypes.State.KNOCKBACK)
+
+func take_damage(weapon_damage: float):
+	animation_player.play("take_damage")
+	health -= weapon_damage
+	if health <= 0.0:
+		queue_free()
