a9b9dee07d209f8ff312731f14bd1858f11e88d6
Shafayat Bin Mamun <mamunshafayatbin@yahoo.com>
2026-02-02 15:31:47 +0600

    12a. Custom health bar

--- ui/health_bar/custom_health_bar.gd
+++ ui/health_bar/custom_health_bar.gd
@@ -7,21 +7,25 @@ class_name CustomHealthBar
 var change_value_tween: Tween
 var opacity_tween: Tween
 
+func _ready() -> void:
+	reset_visibility.timeout.connect(_reset_visibility_timeout)
+
 func _setup_health_bar(max_val: float) :
-	modulate.a = 0.0
+	#modulate.a = 0.0
 	value = max_val
 	max_value = max_val
 	progress_bar.value = max_val
 	progress_bar.max_value = max_val
 	
 func change_value(new_value: float):
-	_change_opacity(1.0)
+	#_change_opacity(1.0)
 	
 	value = new_value
 	
 	if change_value_tween:
 		change_value_tween.kill()
-	change_value_tween.finished.connect(reset_visibility.start)
+	change_value_tween = create_tween()
+	#change_value_tween.finished.connect(reset_visibility.start)
 	change_value_tween.tween_property(progress_bar, "value", new_value, 0.35).set_trans(Tween.TRANS_SINE)
 	
 func _change_opacity(new_amount: float):
@@ -29,4 +33,7 @@ func _change_opacity(new_amount: float):
 		opacity_tween.kill()
 	opacity_tween = create_tween()
 	opacity_tween.tween_property(self, "modulate:a", new_amount, 0.12).set_trans(Tween.TRANS_SINE)
-	
+
+func _reset_visibility_timeout() -> void:
+	pass
+	#_change_opacity(0.0)
