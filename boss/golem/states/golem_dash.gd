d241d9d3ca80fa05fe28932994386466f62d2cff
Shafayat Bin Mamun <mamunshafayatbin@yahoo.com>
2026-02-08 15:34:10 +0600

    14e. Golem Dash State

--- boss/golem/states/golem_dash.gd
+++ boss/golem/states/golem_dash.gd
@@ -1,3 +1,29 @@
 extends State
 
 class_name GolemDash
+
+var boss: CharacterBody2D
+
+const DASH_SPEED: float = 500.0
+const DASH_TIME: float = 0.8
+var dash_timer: float = 0.0
+var dash_dir: Vector2 = Vector2.ZERO
+
+func enter() -> void:
+	boss = state_machine.get_parent()
+	boss.animation_player.play("Glowing")
+	dash_timer = DASH_TIME
+	dash_dir = (boss.player.global_position - boss.global_position).normalized()
+	boss.velocity = dash_dir * DASH_SPEED
+
+func physics_update(delta: float):
+	dash_timer -= delta
+	if dash_timer > 0.0:
+		boss.velocity = dash_dir * DASH_SPEED
+		boss.move_and_slide()
+	else:
+		exit_dash()
+		return
+	
+func exit_dash() -> void:
+	state_machine.change_state(GolemStates.GOLEM_FOLLOW)
